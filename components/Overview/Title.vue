f79bd3a5cb349b515881e3cc6933406902abaa83
Gerome Meyer <gerome.meyer@icloud.com>
2023-06-02 19:42:47 +0200

    Updated image and text

--- components/Overview/Title.vue
+++ components/Overview/Title.vue
@@ -2,10 +2,14 @@
   <Section class="flex justify-between">
     <div>
       <h1 class="text-white text-large md:text-5xl font-bold">Gérôme Meyer</h1>
-      <h2 class="text-white text-sm">Web Developer, Statistician and (sometimes) Gardner</h2>
+      <h2 class="text-white text-sm">Combining Technology and Biology 🍀</h2>
     </div>
     <div>
-      <img class="mt-3 md:mt-1 w-16 md:w-19 rounded-full border-2" :src="require('@/static/head_icon.jpg')" alt="Picture of me"/>
+      <img
+        class="mt-3 md:mt-1 w-16 md:w-19 rounded-full border-2"
+        :src="require('@/static/head_icon.png')"
+        alt="Picture of me"
+      />
     </div>
   </Section>
 </template>
@@ -14,10 +18,8 @@
 import Section from "../Section";
 export default {
   name: "Title",
-  components: {Section}
-}
+  components: { Section },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- components/Overview/Work.vue
+++ components/Overview/Work.vue
@@ -2,16 +2,18 @@
   <Section>
     <SubHeader>About Me</SubHeader>
     <p class="indented text-white mt-3">
-      I am a developer living in Winterthur, Switzerland and like to build things that serve some kind of purpose /
-      solve a problem. The challenge of solving real-world issues with code, data and hardware always manages to
-      excites me.
+      I am a developer living in Winterthur, Switzerland 🇨🇭. I like to loose
+      myself in solving obscure problems. When I'm not doing that, then I'm
+      likely trying to learn about something new.
     </p>
     <p class="indented text-white">
-      In my free time I often play games, though I would like to read more. I also like to go to my school's library
-      when studying for my data science degree.
+      In my free time I watch Anime, read and tend to plants. I am looking to
+      write more, since it seems to be a good way of retaining information!
     </p>
+    <br />
     <p class="indented text-white">
-      If you'd like, you can challenge me to a chess game or ask me any questions via
+      If you'd like, you can challenge me to a chess game or ask me any
+      questions via
       <a href="mailto:gerome.meyer@icloud.com">gerome.meyer@icloud.com</a>.
     </p>
   </Section>
@@ -23,10 +25,8 @@ import Section from "../Section";
 
 export default {
   name: "Work",
-  components: {Section, SubHeader}
-}
+  components: { Section, SubHeader },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
